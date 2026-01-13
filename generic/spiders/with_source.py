847b6c0097d08862c161b7e7ac131e44d0193be4
Tomoyuki Sakurai <y@trombik.org>
2026-01-13 13:54:07 +0700

    feat: Add WithSourceSpider for crawling articles and their sources

---
+++ generic/spiders/with_source.py
@@ -0,0 +1,134 @@
+from typing import List
+from urllib.parse import urlparse
+
+import scrapy
+from pydantic import BaseModel
+from scrapy_spider_metadata import Args
+
+from generic.items import ArticleItem, ArticleWithSourceItem
+from generic.utils import idn2ascii
+
+
+class MyParams(BaseModel):
+    urls: str
+    """ Comma separated list of URLs."""
+
+    text: str = "英語記事"
+    """ A text that indicates source articles"""
+
+    lang: str = "ja"
+    """ The language of the article. """
+
+    src_lang: str = "en"
+    """ The language of source articles. """
+
+
+class WithSourceSpider(Args[MyParams], scrapy.Spider):
+    """
+    Yields an ArticleWithSourceItem from URLs.
+
+    The spider crawls given URLs and scrapes the article and source articles
+    if it finds them.
+
+    Args:
+        urls: Comma-separated string of summary page URLs. Mandatory.
+
+    Yields:
+        ArticleWithSourceItem
+    """
+
+    name = "with-source"
+    allowed_domains = []
+    start_urls = []
+
+    custom_settings = {"ROBOTSTXT_OBEY": False}
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        # parse urls argument
+        for url in self.args.urls.split(","):
+            domain = urlparse(idn2ascii(url)).netloc
+            self.allowed_domains.append(domain)
+            self.logger.debug(f"allowed_domains: {self.allowed_domains}")
+
+    async def start(self):
+        for url in self.args.urls.split(","):
+            yield scrapy.Request(url, self.parse)
+
+    def parse(self, response):
+        """
+        Parse the target page. If the target page has sources, fetch and
+        extract them as ArticleItem. The sources are appended to
+        ArticleWithSourceItem.
+
+        Yields:
+            ArticleWithSourceItem
+
+        """
+        item = ArticleWithSourceItem.from_response(
+            response, lang=self.args.lang
+        )
+
+        # exract a list of href, using a query.
+        query = (
+            "//main//a[contains(., $text) or contains(parent::*, $text)]/@href"
+        )
+        source_hrefs = response.xpath(
+            query,
+            text=self.args.text,
+        ).getall()
+
+        # ensure URLs are absolute.
+        source_urls = [response.urljoin(href) for href in source_hrefs]
+
+        # no source URLs, yield the item.
+        if not source_urls:
+            yield item
+            return
+
+        # ensure unique URLs and pass a list for destructive pop()
+        unique_urls = list(dict.fromkeys(source_urls))
+        yield from self._request_sources(item, list(unique_urls))
+
+    def _request_sources(self, item, urls):
+        if not urls:
+            # no remaining URLs. yield the item.
+            yield item
+            return
+
+        # urls is not empty. yield another Request and create an ArticleItem.
+        next_url = urls.pop(0)
+
+        yield scrapy.Request(
+            next_url,
+            callback=self.parse_source,
+            cb_kwargs={
+                "parent_item": item,
+                "remaining_urls": urls,
+            },
+            dont_filter=True,
+        )
+
+    def parse_source(
+        self,
+        res: scrapy.http.Response,
+        parent_item: ArticleWithSourceItem,
+        remaining_urls: List[str],
+    ):
+        """
+        Parse a source of an article and append a ArticleItem to the parent
+        ArticleWithSourceItem.
+
+        Args:
+            res: The HTTP response
+            parent_item: The article that refers this source article.
+            remaining_urls: A list of remaining URLs.
+
+        """
+
+        # res is a source article. create an ArticleItem.
+        source_item = ArticleItem.from_response(res, lang=self.args.src_lang)
+
+        # append the source article to the parent item.
+        parent_item.sources.append(source_item)
+        yield from self._request_sources(parent_item, remaining_urls)

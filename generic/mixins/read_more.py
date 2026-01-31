e98f7eb4cc6c9b98523259829ee12c26002d4e28
Tomoyuki Sakurai <y@trombik.org>
2026-01-31 14:01:09 +0700

    fix: narrow down the source_parent_contains XPath
    the original too broadly matches and gave false positives

--- generic/mixins/read_more.py
+++ generic/mixins/read_more.py
@@ -5,6 +5,7 @@ from lxml import etree
 
 from generic.items import ArticleItem
 from generic.spiders.base import GenericSpiderConfig
+from generic.utils import get_url_without_fragment
 
 
 class ReadMoreCompatible(Protocol):
@@ -281,7 +282,9 @@ class ReadMoreMixin:
             query = "//a[contains(., $arg)]/@href"
             arg = self.args.source_contains
         elif self.args.source_parent_contains:
-            query = "//a[contains(parent::*, $arg)]/@href"
+            # Matches <a> tags where an ancestor within 2 levels contains
+            # specific text.
+            query = "//a[ancestor::*[position() <= 2][contains(text(), $arg)]]/@href"  # noqa E501
             arg = self.args.source_parent_contains
         # no options for sources. yield item and finish.
         if not query:
@@ -292,7 +295,9 @@ class ReadMoreMixin:
 
         # ensure URLs are absolute.
         source_urls = [res.urljoin(href) for href in source_hrefs]
-        return list(dict.fromkeys(source_urls))
+        # and they are unique
+        uniq_urls = list(set(get_url_without_fragment(u) for u in source_urls))
+        return uniq_urls
 
     def _find_and_request_sources(
         self, res: scrapy.http.Response, item: ArticleItem

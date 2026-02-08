5bdf4f221f303631ae973bd5e6b1c8833b41e3c7
Tomoyuki Sakurai <y@trombik.org>
2026-02-08 10:22:42 +0700

    feat: incorporate ReadMoreMixin into WordPressSpider

--- generic/spiders/wordpress.py
+++ generic/spiders/wordpress.py
@@ -1,30 +1,51 @@
+from typing import Type
 from urllib.parse import urljoin, urlparse
 
 from scrapy.http import Response
 from scrapy.spiders import SitemapSpider
 
-from generic.items import ArticleItem
+from generic.mixins.read_more import ReadMoreMixin, ReadMoreMixinConfig
+from generic.spiders.base import GenericSpider
 from generic.utils import idn2ascii
 
 
-class WordPressSpider(SitemapSpider):
+class WordPressSpiderConfig(ReadMoreMixinConfig):
+    pass
+
+
+class WordPressSpider(
+    SitemapSpider,
+    GenericSpider[WordPressSpiderConfig],
+    ReadMoreMixin,
+):
     """
-    A spider that scrapes all the articles with sitemap.xml.
+    A spider that scrapes all the articles within a sitemap.xml. The
+    sitemap.xml may contain another sitemap.xml (nested sitemap.xml).
+
+    The spider uses ReadMoreMixin and automatically scrapes articles of
+    multiple pages (and implements other goodies like scraping sources).
     """
 
     name = "wordpress"
     custom_settings = {}
 
-    def __init__(self, urls=None, *args, **kwargs):
-        super(WordPressSpider, self).__init__(*args, **kwargs)
+    @classmethod
+    def get_config_class(cls) -> Type[WordPressSpiderConfig]:
+        """
+        Returns the config class for this spider.
+        """
+        return WordPressSpiderConfig
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
 
         self.sitemap_urls = [
-            urljoin(idn2ascii(url), "sitemap.xml") for url in urls.split(",")
+            urljoin(idn2ascii(url), "sitemap.xml") for url in self.args.urls
         ]
         self.allowed_domains = [
             urlparse(url).netloc for url in self.sitemap_urls
         ]
-        self.logger.debug(f"urls: {urls}")
+        self.logger.debug(f"urls: {self.args.urls}")
         self.logger.debug(f"sitemap_urls: {self.sitemap_urls}")
         self.logger.debug(f"allowed_domains: {self.allowed_domains}")
 
@@ -32,5 +53,5 @@ class WordPressSpider(SitemapSpider):
         for entry in entries:
             yield entry
 
-    def parse(self, response: Response) -> ArticleItem:
-        return ArticleItem.from_response(response)
+    def parse(self, response: Response):
+        yield from self.parse_summary_page(response)

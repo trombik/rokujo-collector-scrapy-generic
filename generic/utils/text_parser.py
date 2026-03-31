26ab0db7777548e65772b88a811e1ba3252a8efb
Tomoyuki Sakurai <y@trombik.org>
2026-03-31 17:26:31 +0700

    fix: remove Wikipedia-specific noises

--- generic/utils/text_parser.py
+++ generic/utils/text_parser.py
@@ -56,6 +56,10 @@ class ArticleTextParser:
 
     def _clean(self, text):
         text = re.sub(r"\[編集\]", "", text)
+        text = re.sub(r"\[\d+\]", "", text)
+        text = re.sub(r"\[注\s*\d+\]", "", text)
+        text = re.sub(r"ISBN\s*\d+。?$", "", text)
+        text = re.sub(r"^\^.*", "", text)
         text = re.sub(r"\n", "", text)
         return text.strip()
 
